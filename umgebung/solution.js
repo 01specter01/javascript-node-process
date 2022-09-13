31c2b011cb08407cda7302dc03fd19b35c41881d
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-13 11:43:06 +0200

    4.1 done

--- umgebung/solution.js
+++ umgebung/solution.js
@@ -1,24 +1,31 @@
 console.log("javascript-node-process");
-
-console.log(process.argv);
 console.log(
     "1. Entwickle eine Funktion, welche die aktuell genutzte Node-Version ausgibt."
 );
-console.log(`Version: ` + process.version);
+function node() {
+    return `Version: ` + process.version;
+}
+console.log(node());
 console.log("----------------------");
 console.log(
     "2. Schreibe eine Funktion, die das Betriebssystem (und die Architektur) ausgibt."
 );
-console.log(
-    `This processor architecture is ` +
+function os() {
+    return (
+        `This processor architecture is ` +
         process.arch +
         ` ------------------ ` +
         `The OS is: ` +
         process.platform
-);
+    );
+}
+console.log(os());
 console.log("----------------------");
 
 console.log(
     "3. Finde mit einer Funktion heraus, in welchem Pfad das Skript gerade ausgeführt wird."
 );
-console.log(`Pfad der js datei ist:` + process.argv);
+function directory() {
+    return `Pfad der js datei ist:` + process.argv;
+}
+console.log(directory());
