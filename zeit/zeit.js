2841bee41e820da12ffcb1d2ebb8b76fcf96bb37
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-13 15:12:04 +0200

    3 fehlt noch

--- zeit/zeit.js
+++ zeit/zeit.js
@@ -1,16 +1,20 @@
 console.log("javascript-node-process");
 console.log(`1. Lege eine Funktion an, die die aktuelle Uhrzeit ausgibt`);
-
+let current = new Date();
 function time() {
-    let current = new Date();
     return current.getHours() + "Uhr";
 }
 console.log(time());
 console.log("----------------------");
 console.log("2. Erstelle eine Funktion, die das aktuelle Datum ausgibt");
-
+let now = new Date();
 function datum() {
-    let now = new Date();
     return `${now.getDate()}.0${now.getMonth() + 1}.${now.getFullYear()}`;
 }
 console.log(datum());
+console.log("----------------------");
+console.log(
+    "3. Steuere das Ausgabeformat über ein Argument (YYYY-MM-DD, hh:mm, DD.MM.YY)"
+);
+console.log(current);
+//nummer 3 Unklar!!
