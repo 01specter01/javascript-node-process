8091fd76d42679500a87cd2ce6f5dc6aad981a17
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-13 10:37:27 +0200

    1 done

---
+++ umgebung/solution.js
@@ -0,0 +1,12 @@
+console.log("javascript-node-process");
+
+console.log(process.argv);
+console.log(
+    "1. Entwickle eine Funktion, welche die aktuell genutzte Node-Version ausgibt."
+);
+console.log(`Version: ` + process.version);
+console.log("----------------------");
+console.log(
+    "2. Schreibe eine Funktion, die das Betriebssystem (und die Architektur) ausgibt."
+);
+console.log(process.);
