c9884df5a11708cd2b285b2cbad35a71a14b39f5
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-14 10:00:53 +0200

    r

---
+++ countdown/countdown.js
@@ -0,0 +1,5 @@
+console.log("javascript-node-process");
+console.log(
+    "1. Entwickle eine Funktion, die einen Countdown ausgibt, der von 30 Sekunden herunterzählt. Jede Sekunde soll die verbleibende Zeit ausgegeben werden"
+);
+console.log();
